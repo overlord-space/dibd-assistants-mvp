24c0d990e1198e18f19ef8ca98f411a46f25e5a9
Anton Alekseev <alekseev.ad@dns-shop.ru>
2024-05-15 22:20:25 +0800

    add new model and fix definition

--- app/bots.ts
+++ app/bots.ts
@@ -14,13 +14,19 @@ export const bots: BotVariant[] = [
     },
     {
         name: "1C-1",
-        description: "Модель для тестирования по международным материалам",
+        description: "Модель для тестирования по всем 1с материалам",
         localId: "1c-1",
         assistantId: "asst_ZAhWGis2ohVJEFnBYKUbts30",
     },
     {
+        name: "1C-OnlyLesson",
+        description: "Модель для тестирования по материалам курса обучения 1с за 22 дня",
+        localId: "1C-OnlyLesson",
+        assistantId: "asst_WwEoenAZo04vGkivktALf199",
+    },
+    {
         name: "OVA",
-        description: "Модель для тестирования по международным материалам",
+        description: "Модель для тестирования",
         localId: "2G",
         assistantId: "asst_2nOtgfWbKhwbnetmddj4UW0x",
     },
