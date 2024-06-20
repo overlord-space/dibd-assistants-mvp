10b82506556387567ed9df0ed86fe6b390e7a7ab
Anton Alekseev <alekseev.ad@dns-shop.ru>
2024-06-20 12:23:08 +0800

    Add supra bot

--- app/bots.ts
+++ app/bots.ts
@@ -6,28 +6,35 @@ export type BotVariant = {
 }
 
 export const bots: BotVariant[] = [
+    // {
+    //     name: "INT-1",
+    //     description: "Модель для тестирования по международным материалам",
+    //     localId: "int-1",
+    //     assistantId: "asst_GJs5kUTNjpExxPR6ofTFPvpK",
+    // },
+    // {
+    //     name: "1C-1",
+    //     description: "Модель для тестирования по всем 1с материалам",
+    //     localId: "1c-1",
+    //     assistantId: "asst_ZAhWGis2ohVJEFnBYKUbts30",
+    // },
+    // {
+    //     name: "1C-OnlyLesson",
+    //     description: "Модель для тестирования по материалам курса обучения 1с за 22 дня",
+    //     localId: "1C-OnlyLesson",
+    //     assistantId: "asst_WwEoenAZo04vGkivktALf199",
+    // },
+    // {
+    //     name: "OVA",
+    //     description: "Модель для тестирования",
+    //     localId: "2G",
+    //     assistantId: "asst_2nOtgfWbKhwbnetmddj4UW0x",
+    // },
     {
-        name: "INT-1",
-        description: "Модель для тестирования по международным материалам",
-        localId: "int-1",
-        assistantId: "asst_GJs5kUTNjpExxPR6ofTFPvpK",
+        name: "Supra",
+        description: "Модель для тестирование по материалам Supra",
+        localId: "Supra",
+        assistantId: "asst_qaBlrbswg9PlYYIu0w5uTZOH",
     },
-    {
-        name: "1C-1",
-        description: "Модель для тестирования по всем 1с материалам",
-        localId: "1c-1",
-        assistantId: "asst_ZAhWGis2ohVJEFnBYKUbts30",
-    },
-    {
-        name: "1C-OnlyLesson",
-        description: "Модель для тестирования по материалам курса обучения 1с за 22 дня",
-        localId: "1C-OnlyLesson",
-        assistantId: "asst_WwEoenAZo04vGkivktALf199",
-    },
-    {
-        name: "OVA",
-        description: "Модель для тестирования",
-        localId: "2G",
-        assistantId: "asst_2nOtgfWbKhwbnetmddj4UW0x",
-    },
+    
 ]
